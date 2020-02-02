ce631119fcbc2eb6f854595bd99c0a00fb13686b
sobial <sobhaniali1@gmail.com>
2020-02-02 11:47:50 +0330

    banana in the box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -14,6 +14,7 @@ import { ServersComponent } from './servers/servers.component';
     ServersComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     AlertModule.forRoot()
